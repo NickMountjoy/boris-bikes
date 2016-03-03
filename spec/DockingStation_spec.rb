3ee1391184e01250778fd4cf7de88082213fe5d0
Rhiannon Lolley Neville <rhiannonruth@hotmail.co.uk>
2016-03-03 15:18:29 +0000

    Refactored DockingStation_spec.rb

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,15 +4,15 @@ describe DockingStation do
 	it {expect(subject).to respond_to :release_bike }
 
 	it 'should raise an error when no bikes available' do
-		expect {subject.release_bike}.to raise_error("No bikes available")
+		expect {subject.release_bike}.to raise_error('No bikes available')
 	end
 
 	it 'should raise an error when station is full' do
-		subject.capacity.times{subject.dock(double (:bike))}
-		expect {subject.dock double(:bike)}.to raise_error("Station is full")
+		subject.capacity.times{subject.dock(double(:bike))}
+		expect {subject.dock double(:bike)}.to raise_error('Station is full')
 	end
 
-	it "checks whether release_bike gets a bike" do
+	it 'checks whether release_bike gets a bike' do
 		bike = double(:bike)
 		subject.dock(bike)
 	  expect(subject.release_bike).to eq (bike)
@@ -22,17 +22,16 @@ describe DockingStation do
 		 bike = double(:bike)
 		 bike.report_broken
 		 subject.dock(bike)
-		 expect { subject.release_bike}.to raise_error("No bikes available")
+		 expect { subject.release_bike}.to raise_error('No bikes available')
 	end
 
-		it 'accepts all bikes' do
-			bike = double(:bike)
-			broken_bike = double(:bike)
-			broken_bike.report_broken
-			subject.dock(bike)
-			subject.dock(broken_bike)
-			expect(subject.bikes).to eq([bike, broken_bike])
-		end
-
+	it 'accepts all bikes' do
+		bike = double(:bike)
+		broken_bike = double(:bike)
+		broken_bike.report_broken
+		subject.dock(bike)
+		subject.dock(broken_bike)
+		expect(subject.bikes).to eq([bike, broken_bike])
+	end
 
 end
