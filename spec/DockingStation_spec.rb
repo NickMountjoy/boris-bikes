7d4f5396a2c5b24beb01905d9f1f320247565233
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-03 16:27:11 +0000

    Mock doubles

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,10 @@
 require 'dockingstation'
 
 describe DockingStation do
+
+	let(:bike) {double :bike, broken: false}
+	let(:broken_bike) {double :bike, broken: true}
+
 	it {expect(subject).to respond_to :release_bike }
 
 	it 'should raise an error when no bikes available' do
@@ -8,27 +12,21 @@ describe DockingStation do
 	end
 
 	it 'should raise an error when station is full' do
-		subject.capacity.times{subject.dock(double(:bike))}
-		expect {subject.dock double(:bike)}.to raise_error('Station is full')
+		subject.capacity.times{subject.dock(bike)}
+		expect {subject.dock(bike) }.to raise_error('Station is full')
 	end
 
 	it 'checks whether release_bike gets a bike' do
-		bike = double(:bike)
 		subject.dock(bike)
-	  expect(subject.release_bike).to eq (bike)
+		expect(subject.release_bike).to eq (bike)
 	end
 
 	it 'should not release broken bikes' do
-		 bike = double(:bike)
-		 bike.report_broken
-		 subject.dock(bike)
+		 subject.dock(broken_bike)
 		 expect { subject.release_bike}.to raise_error('No bikes available')
 	end
 
 	it 'accepts all bikes' do
-		bike = double(:bike)
-		broken_bike = double(:bike)
-		broken_bike.report_broken
 		subject.dock(bike)
 		subject.dock(broken_bike)
 		expect(subject.bikes).to eq([bike, broken_bike])
